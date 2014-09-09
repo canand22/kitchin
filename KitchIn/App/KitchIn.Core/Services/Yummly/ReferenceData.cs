5d0ca7681d2bf2cfdcabd13f719dd26fb85de009
yermalex <yermalex@localhost>
2014-09-09 12:06:23 +0300

    add ch. to account edit

---
+++ KitchIn/App/KitchIn.Core/Services/Yummly/ReferenceData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KitchIn.Core.Entities;
+using SmartArch.Data;
+
+namespace KitchIn.Core.Services.Yummly
+{
+    public class ReferenceData
+    {
+        public long Id { set; get; }
+        public string Category { set; get; }
+        public string YummlyName { set; get; }
+        public string FullName { set; get; }
+        public string ShortName { set; get; }
+    }
+}
